a6e5fc84b5eece2dfda03accec40c6187dfcc2b0
Tom Fong <tomfong.dev@gmail.com>
2022-04-29 20:23:35 +0800

    fix(ios): sometimes show patch note again issue

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -38,7 +38,8 @@ export class EnvService {
   public readonly GITHUB_REPO_URL: string = "https://github.com/tomfong/simple-qr";
   public readonly GOOGLE_PLAY_URL: string = "https://play.google.com/store/apps/details?id=com.tomfong.simpleqr";
   public readonly PRIVACY_POLICY: string = "https://www.privacypolicies.com/live/771b1123-99bb-4bfe-815e-1046c0437a0f";
-  public readonly PATCH_NOTE_STORAGE_KEY = "not-show-update-notes-v20300";
+  public readonly PREV_PATCH_NOTE_STORAGE_KEY = "not-show-update-notes-v20300";
+  public readonly PATCH_NOTE_STORAGE_KEY = "not-show-update-notes-v20301";
 
   private _storage: Storage | null = null;
   private _scannedData: string = '';
@@ -67,6 +68,7 @@ export class EnvService {
     this.appVersionNumber = await this.appVersion.getVersionNumber();
     const storage = await this.storage.create();
     this._storage = storage;
+    this._storage.remove(this.PREV_PATCH_NOTE_STORAGE_KEY).catch(err => {});
     this.storageGet("language").then(
       async value => {
         if (value !== null && value !== undefined) {
@@ -111,15 +113,6 @@ export class EnvService {
           this.notShowHistoryTutorial = (value === 'yes' ? true : false);
         } else {
           this.notShowHistoryTutorial = false;
-        }
-      }
-    );
-    this.storageGet(this.PATCH_NOTE_STORAGE_KEY).then(
-      value => {
-        if (value !== null && value !== undefined) {
-          this.notShowUpdateNotes = (value === 'yes' ? true : false);
-        } else {
-          this.notShowUpdateNotes = false;
         }
       }
     );
