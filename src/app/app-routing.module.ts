dc4ab15c3be639dc5db7312f5a6514476225a3be
Tom Fong <tom.fonglamhang@gmail.com>
2022-05-05 20:50:53 +0800

    perf: try preload some page modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,60 +1,62 @@
 import { NgModule } from '@angular/core';
 import { NoPreloading, RouterModule, Routes } from '@angular/router';
+import { CustomPreloadingStrategyService } from './services/custom-preloading-strategy.service';
 
 const routes: Routes = [
   {
     path: '',
+    data: { preload: true },
     loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   },
   {
     path: 'setting-language',
-    loadChildren: () => import('./pages/setting-language/setting-language.module').then( m => m.SettingLanguagePageModule)
+    loadChildren: () => import('./pages/setting-language/setting-language.module').then(m => m.SettingLanguagePageModule)
   },
   {
     path: 'setting-color',
-    loadChildren: () => import('./pages/setting-color/setting-color.module').then( m => m.SettingColorPageModule)
+    loadChildren: () => import('./pages/setting-color/setting-color.module').then(m => m.SettingColorPageModule)
   },
   {
     path: 'setting-record',
-    loadChildren: () => import('./pages/setting-record/setting-record.module').then( m => m.SettingRecordPageModule)
+    loadChildren: () => import('./pages/setting-record/setting-record.module').then(m => m.SettingRecordPageModule)
   },
   {
     path: 'setting-search-engine',
-    loadChildren: () => import('./pages/setting-search-engine/setting-search-engine.module').then( m => m.SettingSearchEnginePageModule)
+    loadChildren: () => import('./pages/setting-search-engine/setting-search-engine.module').then(m => m.SettingSearchEnginePageModule)
   },
   {
     path: 'about',
-    loadChildren: () => import('./pages/about/about.module').then( m => m.AboutPageModule)
+    loadChildren: () => import('./pages/about/about.module').then(m => m.AboutPageModule)
   },
   {
     path: 'setting-vibration',
-    loadChildren: () => import('./pages/setting-vibration/setting-vibration.module').then( m => m.SettingVibrationPageModule)
+    loadChildren: () => import('./pages/setting-vibration/setting-vibration.module').then(m => m.SettingVibrationPageModule)
   },
   {
     path: 'setting-debug',
-    loadChildren: () => import('./pages/setting-debug/setting-debug.module').then( m => m.SettingDebugPageModule)
+    loadChildren: () => import('./pages/setting-debug/setting-debug.module').then(m => m.SettingDebugPageModule)
   },
   {
     path: 'setting-orientation',
-    loadChildren: () => import('./pages/setting-orientation/setting-orientation.module').then( m => m.SettingOrientationPageModule)
+    loadChildren: () => import('./pages/setting-orientation/setting-orientation.module').then(m => m.SettingOrientationPageModule)
   },
   {
     path: 'setting-qr-ecl',
-    loadChildren: () => import('./pages/setting-qr-ecl/setting-qr-ecl.module').then( m => m.SettingQrEclPageModule)
+    loadChildren: () => import('./pages/setting-qr-ecl/setting-qr-ecl.module').then(m => m.SettingQrEclPageModule)
   },
   {
     path: 'setting-auto-brightness',
-    loadChildren: () => import('./pages/setting-auto-brightness/setting-auto-brightness.module').then( m => m.SettingAutoBrightnessPageModule)
+    loadChildren: () => import('./pages/setting-auto-brightness/setting-auto-brightness.module').then(m => m.SettingAutoBrightnessPageModule)
   },
   {
     path: 'setting-start-page',
-    loadChildren: () => import('./pages/setting-start-page/setting-start-page.module').then( m => m.SettingStartPagePageModule)
+    loadChildren: () => import('./pages/setting-start-page/setting-start-page.module').then(m => m.SettingStartPagePageModule)
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: NoPreloading })
+    RouterModule.forRoot(routes, { preloadingStrategy: CustomPreloadingStrategyService })
   ],
   exports: [RouterModule]
 })
--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -14,30 +14,37 @@ const routes: Routes = [
       },
       {
         path: 'landing',
+        data: { preload: true },
         loadChildren: () => import('../landing/landing.module').then(m => m.LandingPageModule)
       },
       {
         path: 'scan',
+        data: { preload: true },
         loadChildren: () => import('../scan/scan.module').then(m => m.ScanPageModule)
       },
       {
         path: 'result',
+        data: { preload: true },
         loadChildren: () => import('../result/result.module').then(m => m.ResultPageModule)
       },
       {
         path: 'history',
+        data: { preload: true },
         loadChildren: () => import('../history/history.module').then(m => m.HistoryPageModule)
       },
       {
         path: 'setting',
+        data: { preload: true },
         loadChildren: () => import('../setting/setting.module').then(m => m.SettingPageModule)
       },
       {
         path: 'generate',
+        data: { preload: true },
         loadChildren: () => import('../generate/generate.module').then(m => m.GeneratePageModule)
       },
       {
         path: 'import-image',
+        data: { preload: true },
         loadChildren: () => import('../import-image/import-image.module').then(m => m.ImportImagePageModule)
       }
     ]
