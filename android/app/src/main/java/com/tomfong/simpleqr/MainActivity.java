c357d5dc83bf50a835cca21ecbea50d3c746496d
Tom Fong <tomfong.dev@gmail.com>
2022-09-16 20:52:44 +0800

    feat: support QS tile for android

--- android/app/src/main/java/com/tomfong/simpleqr/MainActivity.java
+++ android/app/src/main/java/com/tomfong/simpleqr/MainActivity.java
@@ -2,4 +2,20 @@ package com.tomfong.simpleqr;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  static boolean active = false;
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    active = true;
+  }
+
+  @Override
+  public void onStop() {
+    super.onStop();
+    active = false;
+  }
+
+}
